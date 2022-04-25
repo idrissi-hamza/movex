6401e5d7c42997917a860974e18b60b971a39964
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-04-25 20:24:22 +0200

    display trending row

--- components/Row.js
+++ components/Row.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Thumbnail from "./Thumbnail";
 
-const Row = ({ results }) => {
+const Row = ({ title, results }) => {
   return (
-    <div className=" px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
-      {results.map((result) => (
-        <Thumbnail key={result.id} result={result}/>
-      ))}
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className=" flex items-center space-x-0.5 overflow-x-scroll  md:space-x-2.5 md:p-2">
+        {results.map((movie) => (
+          <Thumbnail key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 };
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,26 +1,19 @@
 import React from "react";
-// import Image from "next/image";
+import Image from "next/image";
 
-const Thumbnail = ({ result }) => {
+const Thumbnail = ({ movie }) => {
   // console.log(result);
   return (
-    <div className="p-2 group cursor-pointer transition duration-200  transform sm:hover:scale-105 ">
-      {/* {JSON.stringify(result, null, 2)} */}
-      {/* <Image
-        layout="responsive"
-        src={`${BASE_URL}${result.backdrop_path || result.poster_path}`}
-        width={1920}
-        height={1080} 
-      /> */}
-      <div className="p-2 ">
-        <p className="truncate max-w-md">{result.overview}</p>
-        {/* <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
-        <p className="flex items-center opacity-0 group-hover:opacity-100 ">
-          {result.media_type && `${result.media_type}`}
-          {result.release_date || result.first_air_date}
-          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
-        </p> */}
-      </div>
+    <div
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+         >
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+      />
     </div>
   );
 };
