0002070f6b8c51f7f4690cd670ba4e1bdaf35c46
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-04-30 01:44:46 +0200

    deploy error fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
-module.exports={
-  images:{
-    domains:['links.papareact.com','image.tmdb.org']
+module.exports = {
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
   },
 
   publicRuntimeConfig: {
     // Will be available on both server and client
     apiKey: process.env.API_KEY,
   },
-  // eslint: {
-  //   // Warning: This allows production builds to successfully complete even if
-  //   // your project has ESLint errors.
-  //   ignoreDuringBuilds: true,
-}
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
