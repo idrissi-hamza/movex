e414f5149d2ccb425b9787cb9ffb512d6c74a5bd
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-04-30 02:00:32 +0200

    fix error deploy

--- next.config.js
+++ next.config.js
@@ -7,9 +7,4 @@ module.exports = {
     // Will be available on both server and client
     apiKey: process.env.API_KEY,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+  };
