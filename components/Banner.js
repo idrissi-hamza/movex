1afee73622ba023300489fbd6ce1d6746178a9ee
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-05-06 18:28:16 +0200

    create menu for small screen

--- components/Banner.js
+++ components/Banner.js
@@ -1,18 +1,21 @@
+import Link from "next/link";
 import React from "react";
-import bg from '../public/bg.jpg'
-import Image from "next/image";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Banner = () => {
+  const { user } = useAuthContext();
+
   return (
-  
     <div className=" absolute top-0 -z-10 w-screen   bg-hero-pattern h-[70vh]  sm:h-[70vh] bg-center bg-cover  ">
       <div className="z-40 absolute center md:left-1/4 md:ml-28    max-w-xs  md:max-w-lg lg:max-w-2xl ">
         <h1 className=" text-2xl md:text-4xl lg:text-7xl font-extrabold mb-2 md:mb-4 lg:mb-8 leading-7  ">
           Go ahead and pick your next Movie
         </h1>
         <p className="text-xs md:text-lg lg:text-2xl">
-          With Movex you can navigate over thousands of movies and shows. What
-          are you waiting for?
+          With Movex you can navigate over thousands of movies and shows.
+          {!user && (
+            <span> What are you waiting for? Sign In to get full features.</span>
+          )}
         </p>
       </div>
     </div>
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import { MenuIcon, SearchIcon } from "@heroicons/react/solid";
+import { MenuIcon, SearchIcon,XIcon } from "@heroicons/react/solid";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 import { useRouter } from "next/router";
@@ -9,6 +9,7 @@ import { useCtrlContext } from "../hooks/useCtrlContext";
 import Link from "next/link";
 
 const Header = () => {
+  const [menu, setMenu] = useState(false);
   //MUI POPOVER COMPONENT
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClick = (event) => {
@@ -39,14 +40,14 @@ const Header = () => {
 
   const { watchList } = useCtrlContext();
   return (
-    <header className="bg-neutral-900/70 h-12 m-2 rounded-md flex items-center pl-4 pr-5 md:pr-8">
-      <h1
+    <header className="bg-neutral-900/70 h-12 m-2 rounded-md flex items-center pl-4 pr-5 md:pr-8 relative">
+     {!menu &&  <h1
         onClick={() => router.push("/")}
         className="text-xl text-white font-bold tracking-wide select-none cursor-pointer pb-[2px] mr-4"
       >
         MOVE<span className="text-yellow-300">X</span>
-      </h1>
-      <div className="group relative flex-1 sm:flex-none mr-4 ">
+      </h1>}
+     {!menu && <div className="group relative flex-1 sm:flex-none mr-4 ">
         <SearchIcon className=" absolute w-5 h-5  top-2  mx-2 text-gray-900" />
         <form onSubmit={search}>
           <input
@@ -58,7 +59,7 @@ const Header = () => {
             onChange={handleInputs}
           />
         </form>
-      </div>
+      </div>}
       <nav className="ml-auto hidden md:flex ">
         <ul className="flex space-x-4 font-bold last:mr-4 items-center">
           <li className=" navBtn" onClick={() => router.push("/")}>
@@ -68,16 +69,14 @@ const Header = () => {
             DISCOVER
           </li>
           {user && (
-            
-              <Link href='/watchlist'
-                className="navBtn "
-                aria-describedby={id}
-                onClick={handleClick}
-              >
-                WATCHLIST
-              </Link>
-              
-            
+            <Link
+              href="/watchlist"
+              className="navBtn "
+              aria-describedby={id}
+              onClick={handleClick}
+            >
+              WATCHLIST
+            </Link>
           )}
         </ul>
         {!user && (
@@ -97,7 +96,54 @@ const Header = () => {
         )}
       </nav>
 
-      <MenuIcon className="w-8 h-8  mx-2 ml-auto block md:hidden " />
+      {!menu && <MenuIcon
+        onClick={() => setMenu((prev) => !prev)}
+        className="w-8 h-8  mx-2 ml-auto block md:hidden -mr-2 "
+      />}
+      {menu && (
+        <>
+          <nav className="mr-auto flex md:hidden text-sm  ">
+            <ul className="flex space-x-4 font-bold last:mr-4 items-center">
+              <li className=" navBtn" onClick={() => router.push("/")}>
+                HOME
+              </li>
+              <li className=" navBtn" onClick={() => router.push("/discover")}>
+                DISCOVER
+              </li>
+              {user && (
+                <Link
+                  href="/watchlist"
+                  className="navBtn "
+                  aria-describedby={id}
+                  onClick={handleClick}
+                >
+                  WATCHLIST
+                </Link>
+              )}
+            </ul>
+            {!user && (
+              <button
+                className="ml-4 navBtn"
+                onClick={() => {
+                  router.push("/login");
+                }}
+              >
+                Sign In
+              </button>
+            )}
+            {user && (
+              <button className="ml-4 navBtn text-yellow-500" onClick={logout}>
+                Logout
+              </button>
+            )}
+            
+          </nav>
+          {menu && <XIcon
+        onClick={() => setMenu((prev) => !prev)}
+        className="w-8 h-8   block md:hidden -mr-2 "
+      />}
+        </>
+      )}
     </header>
   );
 };
